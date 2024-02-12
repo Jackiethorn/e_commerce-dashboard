947d9b06f9017f931eea08d33506a8e4e8cfd460
Jackie Treehorn <jtreehorn9@gmail.com>
2024-02-12 04:46:20 +0300

    fix: implement root layout to be rendered as the parent layout

--- src/pages/router/router.tsx
+++ src/pages/router/router.tsx
@@ -5,18 +5,21 @@ import { Team } from "../Team";
 import { Customers } from "../Customers";
 import { ProductRoutes } from "./ProductRoutes";
 import { OrderRoutes } from "./OrderRoutes";
+import { RootLayout } from "../layouts/RootLayout";
 
 
 
 const renderRoutes = () => {
     return (
         <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="/products/*" element={<ProductRoutes />} />
-            <Route path="/orders/*" element={<OrderRoutes />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="*" element={<NotFound />} />
+            <Route element={<RootLayout />}>
+                <Route path="" element={<Home />} />
+                <Route path="/products/*" element={<ProductRoutes />} />
+                <Route path="/orders/*" element={<OrderRoutes />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
         </Routes>
     )
 }
