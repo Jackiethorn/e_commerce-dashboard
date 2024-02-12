8e7c7a88803fe8231f38d0639a75b6fe12b98606
Jackie Treehorn <jtreehorn9@gmail.com>
2024-02-12 05:03:16 +0300

    create: footer component boilerplate.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <div className="footer w-full h-16 bg-gray-800 text-white">
+            <div className="footer-content">
+                <h1 className="text-2xl font-bold">Footer</h1>
+            </div>
+        </div>
+    );
+}
