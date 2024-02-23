2b350966a6f6c9a9b8da2003eae040309352008c
Jackie Treehorn <jtreehorn9@gmail.com>
2024-02-23 17:09:38 +0300

    CREATE: Mock data for team

---
+++ src/types/team-members.types.ts
@@ -0,0 +1,10 @@
+export type TeamMemberType = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  total_sale: number;
+  thisMonths_sale: number;
+  age: number;
+  icon: string;
+  email: string;
+};
