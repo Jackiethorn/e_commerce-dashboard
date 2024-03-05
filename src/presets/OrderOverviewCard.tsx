b9d44e501be38872670cc7d34676f946c4e83b4c
Jackie Treehorn <jtreehorn9@gmail.com>
2024-03-05 05:25:01 +0300

    create: order overview card layout for homepage

---
+++ src/presets/OrderOverviewCard.tsx
@@ -0,0 +1,47 @@
+import { LineChart } from "@/components/charts/LineChart";
+
+function sumElements(arr: number[]) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+const itemClasses = "flex items-center gap-3 py-2 px-3 rounded-md shadow-md w-56 bg-slate-200 bg-opacity-50";
+const iconClasses = "p-3 bg-white bg-clip-padding rounded-md shadow-md w-12 h-12";
+
+// TODO: NEEDS A LOT OF WORK ALREADY BUT DONT FORGET TO MAKE A POPOVER FOR THE SIDEITEMS.
+export const orderOverviewCard = (labels: any, data: any) => ({
+    title: "Order Overview",
+    heading: "Order Overview",
+    render: function () {
+        return (
+            <div className="flex flex-col justify-center gap-3 py-2.5 px-1">
+                <h2 className="text-secondary_clamp">{this.heading}</h2>
+                <div className="flex gap-3">
+                    <LineChart data={data} labels={labels} />
+                    <div className="flex flex-col self-center gap-3">
+                        <div className={itemClasses}>
+                            <img src="/assets/icons/sidebar_link-orders.svg" alt="orders icon" className={iconClasses} />
+                            <div>
+                                <h2 className="text-primary_clamp opacity-70">Orders</h2>
+                                <span className="font-semibold">{sumElements(data[2])}</span>
+                            </div>
+                        </div>
+                        <div className={itemClasses}>
+                            <img src="/assets/icons/earning.svg" alt="earning icon" className={iconClasses} />
+                            <div>
+                                <h2 className="text-primary_clamp opacity-70">Earning</h2>
+                                <span className="font-semibold">${sumElements(data[1])}</span>
+                            </div>
+                        </div>
+                        <div className={itemClasses}>
+                            <img src="/assets/icons/refund.svg" alt="earning icon" className={iconClasses} />
+                            <div>
+                                <h2 className="text-primary_clamp opacity-70">Refunds</h2>
+                                <span className="font-semibold">{sumElements(data[0])}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    },
+})
