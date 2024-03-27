77452233e8af5607f3351c809f4d0a0de04a1509
Jackie Treehorn <jtreehorn9@gmail.com>
2024-03-28 01:38:04 +0300

    feat: seed team members data to supabase db

--- src/resources/data/index.js
+++ src/resources/data/index.js
@@ -1,8 +1,9 @@
 //THIS FILE SEEDS THE SUPABASE DB
 import { supabase } from "../../utils/supabase.ts";
 import { productsData } from "./products.ts";
+import { teamMembersData } from "./teamMembers.ts";
 
-export async function seedProducts() {
+async function seedProducts() {
   try {
     for (const product of productsData) {
       // Remove 'total', 'skip', and 'limit' properties from the object
@@ -21,4 +22,23 @@ export async function seedProducts() {
   }
 }
 
+async function seedTeamMembers() {
+  try {
+    for (const teamMember of teamMembersData) {
+      const { data, error } = await supabase
+        .from("teamMembers")
+        .insert([teamMember]);
+      console.log("teamMember", teamMember, "data", data, "error", error);
+      if (error) {
+        console.error("Error inserting teamMember:", error.message);
+      } else {
+        console.log("Team member inserted successfully:", data);
+      }
+    }
+  } catch (error) {
+    console.error("Error inserting team members:", error.message);
+  }
+}
+
 seedProducts();
+seedTeamMembers();
